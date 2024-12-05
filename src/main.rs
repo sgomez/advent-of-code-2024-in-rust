2951e3b0e818e117f1124c820c07e80915bc492c
Sergio Gómez <decano@gmail.com>
2024-12-05 19:07:25 +0100

    feat: day 04

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 mod utils;
 
 use std::env;
@@ -11,13 +12,14 @@ fn main() {
     let day_to_run = if args.len() > 1 {
         args[1].parse::<u32>().unwrap_or(24)
     } else {
-        3
+        4
     };
 
     match day_to_run {
         1 => day01::run(),
         2 => day02::run(),
         3 => day03::run(),
+        4 => day04::run(),
         _ => eprintln!("Error: day {} not implemented.", day_to_run), // Manejo de errores
     }
 }
